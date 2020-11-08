f83ea757d711a98a58bc92f0bc4cf49fb5cb851e
DefensivLord <DefensivLord marcel.kaempchen@gmail.com>
2020-11-08 21:17:23 +0100

    Removed duplicated method

--- ChessAI/src/main/Main.java
+++ ChessAI/src/main/Main.java
@@ -1,13 +1,14 @@
 package main;
 
 import chess.Board;
+import display.Display;
 import gfx.Assets;
 import ui.objects.UIBoardPanel;
 
 public class Main {
 	public static void main(String[] args) {
 		Assets.init();
-		display.Display d = new display.Display("Chess", 800, 800);
+		Display d = new Display("Chess", 800, 800);
 
 		UIBoardPanel boardPanel = new UIBoardPanel();
 		boardPanel.setBounds(0, 0, 800, 800);
